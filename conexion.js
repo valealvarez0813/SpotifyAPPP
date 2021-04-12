5ecc7293126fa68e3f32827c4e549c3dfc8d9c63
valealvarez0813 <81313722+thelastcode4@users.noreply.github.com>
2021-04-12 16:55:28 -0500

    audio

--- conexion.js
+++ conexion.js
@@ -1,6 +1,6 @@
-let url="https://api.spotify.com/v1/artists/5C4PDR4LnhZTbVnKWXuDKD/top-tracks?market=US";
+let url="https://api.spotify.com/v1/artists/49Z1AvGeUaBSanPaOmplK6/top-tracks?market=ES";
 
-let token="Bearer BQCvbpWTFH5yQ1hGWOjJh1BZ1_jdl-huqL6EMLfLmkQL6ipBL0wmedYvVunvpQGtKUUHWC5vQk8y6IShcYFdeLBEfXGMIwlyYv37ZGQoQk0sHAOxc8lcgGqJ3zuHT9MRbZ6iwma3wci6Yoc_FEoutjEuHC-BHW4";
+let token="Bearer BQD2OBZI8NeGkWqQqwT_zHsFndQoK0VxiP5YjlT_R04VPdE4hncuKyIeM4cGwb2W8MGfpbIVSPCXegUzxpXqzHqgJsX0EpGXY21wd61rpK78alBaSnVON3Gd4VW-m9QTZdGMeT5HWLbfJ5FdZNLa8vhg6mHY8Eo";
 
 let parametros={
     method:"GET",
@@ -12,5 +12,11 @@ fetch(url,parametros)
 .then(datos => seleccionarInformacion(datos))
 
 function seleccionarInformacion(datos){
-    console.log(datos);
+   
+    console.log(datos.tracks[0].album.images[0].url);
+    console.log(datos.tracks[0].preview_url);
+    console.log(datos.tracks[0].name);
+
+    let cancion=document.getElementById("canciones1");
+    cancion.src=datos.tracks[0].preview_url;
 }
---
+++ prueba.js
@@ -0,0 +1,10 @@
+let cajon=["carne","tomate","lechuga","cebolla","mostaza","salsa de tomate","queso","pan"];
+console.log(cajon);
+let bolsa={
+    proteinas:"carne",
+    vegetales:["cebolla","tomate","lechga"],
+    complementos:["salsa de tomate","queso","mmostaza"],
+    carbohidratos:"pan",
+    
+}
+console.long(bolsa.proteinas[1]);
